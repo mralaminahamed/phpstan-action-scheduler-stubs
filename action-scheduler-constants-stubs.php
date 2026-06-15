4ebe50843ad0d559649182fe169c695e5c2ec7a1
Al Amin Ahamed <mrabir.ahamed@gmail.com>
2026-06-15 13:40:44 +0600

    Generate stubs for Action Scheduler 3.4.2

--- action-scheduler-constants-stubs.php
+++ action-scheduler-constants-stubs.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Generated stub declarations for Action Scheduler.
+ * @see https://actionscheduler.org
+ * @see https://github.com/mralaminahamed/phpstan-action-scheduler-stubs
+ */
 
-// This file is generated. Do not edit it manually.
-// Run `composer generate` to regenerate.
